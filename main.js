d04f63a42ae7913ba1d6514b26d708d81b318a01
Hermyo <yohann.hermeline@gmail.com>
2018-09-07 14:09:21 +0200

    last things to work

--- main.js
+++ main.js
@@ -1,36 +1,60 @@
 $(document).ready(function () {
-    let buzzer = $("#buzzer")[0];
-    let count = parseInt($("#num").html());
-    let breakTime = parseInt($("#breakNum").html());
+    var buzzer = $("#buzzer")[0];
+    var count = parseInt($("#num").html());
+    var breakTime = parseInt($("#breakNum").html());
 
     $("#reset").hide();
     $("#start").click(function () {
-        let counter = setInterval(timer, 1000);
+        var counter = setInterval(timer, 1000);
+        count *= 60;
 
         function timer() {
             //Hide variables
             $("#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakNum, #title1, #title2").hide();
+            $("#timeType").show();
             $("#timeType").html("Session Time: ");
             count -= 1;
             if (count === 0) {
                 buzzer.play();
                 clearInterval(counter);
-                let startBreak = setInterval(breakTimer, 1000);
+                var startBreak = setInterval(breakTimer, 1000);
                 $("#num").hide();
             }
-            $("#num").html(count);
+            if (count % 60 >= 10) {
+                $("#num").html(Math.floor(count / 60) + ":" + count % 60);
+            } else {
+                $("#num").html(Math.floor(count / 60) + ":" + "0" + count % 60);
+            }
 
             function breakTimer() {
                 $("#timeType").html("Break Time: ");
                 $("#breakNum").show();
+                breakTime *= 60;
+                $("#timeType").show();
                 breakTime -= 1;
                 if (breakTime === 0) {
                     clearInterval(startBreak);
+                    buzzer.play();
+                    $("#reset").show();
+                    $("#breakNum, #breakType").hide();
                 }
-                $("#breakNum").html(breakTime);
+                if (breakTime % 60 >= 10) {
+                    $("#breakNum").html(Math.floor(breakTime / 60) + ":" + breakTime % 60);
+                } else {
+                    $("#breakNum").html(Math.floor(breakTime / 60) + ":" + "0" + breakTime % 60);
+                }
             }
         }
 
+    });
+
+    $("#reset").click(function () {
+        count = 25;
+        breakTime = 25;
+        $("#num").html(count);
+        $("#breakNum").html(breakTime);
+        $("#start, #minus5Clock, #add5Clock, #minus5Break, #add5Break, #breakNum, #num, #title1, #title2").show();
+        $("#reset, #timeType").hide();
     });
 
     $("#minus5Clock").click(function () {
